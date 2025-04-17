3e1a5beb4a25ff360b00465921ff41c220289e4d
Denislav-I7 <denislavivanov873@gmail.com>
2025-04-17 11:17:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel.Design;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.JavaScript;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace _9_klas
+{
+
+    class Family
+    {
+        public Person[] People;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3 9 5 1 9 7 4 9
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int max = int.MinValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+                Console.WriteLine($"Най голям елемент: {max}");
+
+            
+             int sum = 0;
+            for (int i = 0;i < array.Length; i++)
+            {
+                sum = sum + array[i];
+                
+            }
+            Console.WriteLine($"Сума на елемнтите: {sum}");
+
+        }
+       
+
+
+    }
+}
